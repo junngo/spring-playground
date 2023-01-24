6aa80691e4164b62e85a7270624d6fa841e21642
MyeongJun Go <myeongjun.ko@gmail.com>
2023-01-24 17:23:55 +0900

    using the scope of prototype bean

---
+++ src/test/java/com/hello/springplayground/inflearn/scope/SingletonWithPrototypeTest1.java
@@ -0,0 +1,91 @@
+package com.hello.springplayground.inflearn.scope;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.Scope;
+
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class SingletonWithPrototypeTest1 {
+
+    /**
+     * 일반적인 PrototypeBean 테스트
+     */
+    @Test
+    void prototypeFind() {
+        AnnotationConfigApplicationContext ac = new
+                AnnotationConfigApplicationContext(PrototypeBean.class);
+
+        PrototypeBean prototypeBean1 = ac.getBean(PrototypeBean.class);
+        prototypeBean1.addCount();
+        assertThat(prototypeBean1.getCount()).isEqualTo(1);
+
+        PrototypeBean prototypeBean2 = ac.getBean(PrototypeBean.class);
+        prototypeBean2.addCount();
+        assertThat(prototypeBean2.getCount()).isEqualTo(1);
+    }
+
+
+    /**
+     * PrototypeBean이 싱글톤 내부에서 사용되는 경우는 Prototype 으로 동작하지 않고
+     * 이미 주입이 끝났기 때문에 싱글톤으로 동작한다.
+     * -> 싱글톤 내에서 Prototype 빈으로써 사용을 하고 싶다면 Provider 로 해결
+     */
+    @Test
+    void singletonClientUsePrototype() {
+        AnnotationConfigApplicationContext ac = new
+                AnnotationConfigApplicationContext(ClientBean.class, PrototypeBean.class);
+
+        ClientBean clientBean1 = ac.getBean(ClientBean.class);
+        int count1 = clientBean1.logic();
+        assertThat(count1).isEqualTo(1);
+
+        ClientBean clientBean2 = ac.getBean(ClientBean.class);
+        int count2 = clientBean2.logic();
+        assertThat(count2).isEqualTo(2);
+    }
+
+    /**
+     * 싱글톤 스코프 생략 가능하나 명확하게 보여지기 위해서 명시
+     * 내부에는 프로토타입 스코프를 사용하는 빈 사용 중
+     */
+    @Scope("singleton")
+    static class ClientBean {
+        private final PrototypeBean prototypeBean;
+
+        public ClientBean(PrototypeBean prototypeBean) {
+            this.prototypeBean = prototypeBean;
+        }
+
+        public int logic() {
+            prototypeBean.addCount();
+            int count = prototypeBean.getCount();
+            return count;
+        }
+    }
+
+    @Scope("prototype")
+    static class PrototypeBean {
+        private int count = 0;
+        public void addCount() {
+            count++;
+        }
+        public int getCount() {
+            return count;
+        }
+
+        @PostConstruct
+        public void init() {
+            System.out.println("PrototypeBean.init " + this);
+        }
+
+        @PreDestroy
+        public void destroy() {
+            System.out.println("PrototypeBean.destroy");
+        }
+    }
+}
